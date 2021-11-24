691060b3adf1f26252ab85eff76e22de778ee9f4
Julien M <julien.marcesse@gmail.com>
2021-11-24 21:50:14 +0100

    initialisation

---
+++ src/main/java/com/centropyebicolor/reefmeasures/ReefmeasuresApplication.java
@@ -0,0 +1,13 @@
+package com.centropyebicolor.reefmeasures;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReefmeasuresApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReefmeasuresApplication.class, args);
+    }
+
+}
