59814aaceade95789978d93607825a619afbe2c1
Julien M <julien.marcesse@gmail.com>
2021-11-30 00:15:35 +0100

    new controller

--- src/main/java/com/centropyebicolor/reefmeasures/controller/DBController.java
+++ src/main/java/com/centropyebicolor/reefmeasures/controller/DBController.java
@@ -1,11 +1,10 @@
 package com.centropyebicolor.reefmeasures.controller;
 
-import com.centropyebicolor.reefmeasures.bean.WaterTemperature;
-import com.centropyebicolor.reefmeasures.dto.WaterTemperatureDTO;
+import com.centropyebicolor.reefmeasures.bean.*;
+import com.centropyebicolor.reefmeasures.dto.MeasureDTO;
+import com.centropyebicolor.reefmeasures.helpers.MeasureForm;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Date;
 
@@ -14,7 +13,7 @@ public class DBController {
 
 
     @Autowired
-    WaterTemperatureDTO waterTemperatureDTO;
+    MeasureDTO measureDTO;
 
     @GetMapping(path = "/addWaterTemperature/{measure}")
     public WaterTemperature addWaterTemperature(@PathVariable String measure){
@@ -22,10 +21,42 @@ public class DBController {
         WaterTemperature data = new WaterTemperature();
         data.setDate(new Date());
         data.setValue(Float.parseFloat(measure));
-        waterTemperatureDTO.save(data);
+        measureDTO.save(data);
         System.out.println("Data has been saved : " + data);
         return data;
 
+    }
+
+    @PostMapping(path="/addSingleMeasure")
+    public Measure addSingleMeasure(@RequestBody MeasureForm measureForm){
+        Measure measure ;
+        switch (measureForm.getKind()){
+            case WATERTEMPERATURE:
+                measure = new WaterTemperature();
+                break;
+            case AIRHUMIDITY:
+                measure = new AirHumidty();
+                break;
+            case AIRTEMPERATURE:
+                measure = new AirTemperature();
+                break;
+            case REDOX:
+                measure = new Redox();
+                break;
+            case PH:
+                measure = new Ph();
+                break;
+            default:
+                throw new IllegalStateException("Unexpected value: " + measureForm.getKind());
+        }
+
+        if(measureForm.getDate() != null){
+            measure.setDate(measureForm.getDate());
+
+        }
+        measure.setValue(measureForm.getValue());
+        measureDTO.save(measure);
+        return measure;
     }
 
 }
